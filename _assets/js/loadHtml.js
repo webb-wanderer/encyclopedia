7d733861989b22442687ccaa0601344795fc938c
webb-wanderer <140278726+webb-wanderer@users.noreply.github.com>
2024-11-08 17:31:47 -0800

    fix bootstrap3_player.js code

--- _assets/js/loadHtml.js
+++
@@ -1,7 +0,0 @@
-/*jshint esversion: 6 */
-function loadHtml(selector, path)
-{
-    fetch(path)
-        .then((response) => response.text())
-        .then((text) => (document.querySelector(selector).innerHTML = text));
-}
