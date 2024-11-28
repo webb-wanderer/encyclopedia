571f459cdc972a09780b4f985a2541687e11637a
webb-wanderer <140278726+webb-wanderer@users.noreply.github.com>
2024-11-28 14:14:42 -0800

    tiny change to search item

--- _assets/js/highlighting.js
+++ _assets/js/highlighting.js
@@ -1,24 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // Function to get the current top folder
   function getCurrentTopFolder() {
-	return window.location.pathname
-	  .replace(/^\/|\/$/g, '')
-	  .split('/')[0];
+    return window.location.pathname.replace(/^\/|\/$/g, "").split("/")[0];
   }
 
   // Get the current top folder
   const currentTopFolder = getCurrentTopFolder();
 
   // Select all links in the navigation
-  const navLinks = document.querySelectorAll('nav > p > a');
+  const navLinks = document.querySelectorAll("nav#nav-he > p > a");
 
   // Iterate through navigation links
-  navLinks.forEach(link => {
-	// Check if the link's href contains the current top folder
-if (link.getAttribute('href').startsWith(`/${currentTopFolder}/`)) {
-	  link.classList.add('active');
-	} else {
-	  link.classList.remove('active');
-	}
+  navLinks.forEach((link) => {
+    // Check if the link's href contains the current top folder
+    if (link.getAttribute("href").startsWith(`/${currentTopFolder}/`)) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
   });
 });
